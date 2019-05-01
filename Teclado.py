c27b3178bc7ba2f2d867264c653ba7215be51eb1
l1salvatore <lucas_salva_15@hotmail.com>
2019-05-01 15:14:00 -0300

    updating files..

--- Teclado.py
+++ Teclado.py
@@ -1,7 +1,7 @@
 ﻿import USB,evdev,threading,sys,time
 from collections import defaultdict
 
-{- Merge test -}  dada ñakañaka AAAAAAAAAAAAAAAra
+{- Merge test -}  dada ñakañaka AAAAAAAAAAAAAAAra SARASAAAA
 
 teclas = { evdev.ecodes.KEY_A : 'A',
 	   evdev.ecodes.KEY_B : 'B',
