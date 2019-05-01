1eb6741fc097c49150f4e06be71bf5169980b88e
l1salvatore <lucas_salva_15@hotmail.com>
2019-05-01 15:27:57 -0300

    Testing

--- USB.py
+++ USB.py
@@ -1,12 +1,1 @@
-import evdev,os,signal,sys
-
-
-class usb:
-	def __init__(self,dev):
-		self.device = evdev.InputDevice(dev)
-		print(self.device)
-	def obtener_evento(self):
-		for event1 in self.device.read_loop():
-		   # for event2 in self.device.read_loop():
-			if event1.type == evdev.ecodes.EV_KEY and event1.value == 1:
-				 return event1.code
+SARASAAAASASA
